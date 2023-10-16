4b852b6894ba7a362bf3bf61860a09e4ba514597
nicofitzpatrick <nicofitzpat@gmail.com>
2023-10-16 22:16:25 +1300

    update text contrast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="flex h-screen w-full flex-col justify-between bg-black px-5 py-10 lg:px-20 lg:py-20">
-        <div className="text-green-800">
+        <div className="text-green-700">
           <h1 className="text-[40px] lg:text-[80px]">Nico Fitzpatrick</h1>
           <h2 className="text-[20px] lg:text-[30px]">Web Developer</h2>
           <h3 className="text-[16px] lg:text-[20px]">Auckland, New Zealand</h3>
